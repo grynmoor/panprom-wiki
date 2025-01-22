6523b352f10aca7bf7b9a0df63b05bbb97ddd2c9
grynmoor <grynmoor@gmail.com>
2025-01-21 19:07:27 -0600

    Quartz sync: Jan 21, 2025, 7:07 PM

--- wikisync.bat
+++ wikisync.bat
@@ -1,3 +1,4 @@
 cd C:\Users\grynm\quartz
 call robocopy "C:\Users\grynm\Documents\PanProm! Wiki" "C:\Users\grynm\quartz\contents" /mir
 call npx quartz sync
+call robocopy "C:\Users\grynm\quartz\contents" "C:\Users\grynm\Documents\PanProm! Wiki" /mir
